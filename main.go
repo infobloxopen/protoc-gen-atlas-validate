82471d911b11b5620867d46d112abcb9d143cf3e
binjip978 <binjip978@gmail.com>
2021-09-09 12:57:09 +0300

    stub for plugin generate

--- main.go
+++ main.go
@@ -1,12 +1,46 @@
 package main
 
 import (
-	"github.com/gogo/protobuf/vanity/command"
-	"github.com/infobloxopen/protoc-gen-atlas-validate/plugin"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"google.golang.org/protobuf/compiler/protogen"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/pluginpb"
 )
 
 func main() {
-	plugin := &plugin.Plugin{}
-	response := command.GeneratePlugin(command.Read(), plugin, ".pb.atlas.validate.go")
-	command.Write(response)
+	// response := command.GeneratePlugin(command.Read(), plugin, ".pb.atlas.validate.go")
+
+	input, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		panic(err)
+	}
+
+	var request pluginpb.CodeGeneratorRequest
+	err = proto.Unmarshal(input, &request)
+	if err != nil {
+		panic(err)
+	}
+
+	opts := protogen.Options{}
+
+	plugin, err := opts.New(&request)
+	if err != nil {
+		panic(err)
+	}
+
+	resp := generate(plugin)
+	out, err := proto.Marshal(resp)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Fprint(os.Stdout, string(out))
+}
+
+func generate(plugin *protogen.Plugin) *pluginpb.CodeGeneratorResponse {
+	fmt.Fprintf(os.Stderr, "running generate\n")
+	return &pluginpb.CodeGeneratorResponse{}
 }
